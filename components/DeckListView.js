f0eb54b9b36d6cc3c7b5ee6158c19dbab7c9539f
Marcelo Morais <marcelo.morais@symbio.com>
2017-11-22 14:54:39 +0100

    Adding DeckListView and NewDeck components
    Signed-off-by: Marcelo Morais <marcelo.morais@symbio.com>

---
+++ components/DeckListView.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, Platform } from 'react-native'
+
+class DeckListView extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          DeckListView
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default DeckListView
---
+++ components/NewDeck.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, Platform } from 'react-native'
+
+class NewDeck extends Component{
+  render() {
+    return (
+      <View>
+        <Text>
+          NewDeck
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default NewDeck

97983255059324529aa1a4dd2c960a41a71f88e6
Marcelo Morais <marcelo.morais@symbio.com>
2017-11-22 16:51:22 +0100

    Adding tabnavigator and status bar
    Signed-off-by: Marcelo Morais <marcelo.morais@symbio.com>

--- App.js
+++ App.js
@@ -1,14 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation'
+import { purple, white, blue } from './utils/colors'
+import { Constants } from 'expo'
+import { FontAwesome, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
+
+
 import NewDeck from './components/NewDeck'
 import DeckListView from './components/DeckListView'
+
+function CardsStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  )
+}
+
+const Tabs = TabNavigator({
+  DeckListView: {
+    screen: DeckListView,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards' size={30} color={tintColor} />
+    }
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add New Deck',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  },
+},
+{
+  navigationOptions: {
+    header: null
+  }
+},
+{
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <DeckListView />
-        <NewDeck />
+      <View style={{flex: 1}}>
+        <CardsStatusBar backgroundColor={blue} barStyle='light-content'/>
+        <Tabs />
       </View>
     );
   }

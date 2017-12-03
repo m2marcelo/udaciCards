99f85264674aa4ea026225129b9a0411d5e59c69
Marcelo Morais <marcelo.morais@symbio.com>
2017-12-03 17:13:28 +0100

    Adding data to DeckListView component
    Signed-off-by: Marcelo Morais <marcelo.morais@symbio.com>

--- components/DeckListView.js
+++ components/DeckListView.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Platform } from 'react-native'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  FlatList,
+  DeviceEventEmitter
+} from 'react-native'
+import { getDecks } from '../utils/api';
+import DeckItem from './DeckItem';
 
 class DeckListView extends Component {
+  state = {
+      decks: null
+  };
+
+  componentWillMount() {
+      DeviceEventEmitter.addListener('onDataChangedEvent', this.loadDecks);
+  }
+
+  componentDidMount() {
+      this.loadDecks();
+  }
+
+  componentWillUnmount() {
+      DeviceEventEmitter.removeListener('onDataChangedEvent', this.loadDecks);
+  }
+
+  loadDecks = () => getDecks().then(data => this.setState({decks: data}));
+
   render() {
+    const { decks } = this.state;
+
+    if (!decks) {
+      return (<Text>There is no deck</Text>);
+    }
+
+    const data = Object.keys(decks).map(keyId => {
+      return {
+        ...decks[keyId],
+        key: keyId
+      }
+    });
+
     return (
-      <View>
-        <Text>
-          DeckListView
-        </Text>
-      </View>
+      <FlatList
+        style={{flex: 1}}
+        data={data}
+        renderItem={
+          ({item}) => (
+          <DeckItem onPressItem={console.log('decks pressed')} item={item}/>)
+        }
+      />
     )
   }
 }

cc8e0f84bda2a7733c30c5447a4145ec0784f023
Marcelo Morais <marcelo.morais@symbio.com>
2017-12-03 15:36:13 +0100

    Adding raw content to add new deck screen
    Signed-off-by: Marcelo Morais <marcelo.morais@symbio.com>

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,16 +1,61 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Platform } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Platform } from 'react-native'
+import { black, white } from '../utils/colors'
+import TextButton from './TextButton'
 
 class NewDeck extends Component{
   render() {
     return (
-      <View>
-        <Text>
-          NewDeck
+      <View style={styles.container}>
+        <Text style={styles.newDeckHeader}>
+          What is the title of your new deck?
         </Text>
+        <TextInput
+          style={styles.deckTitle}
+          placeholder="Deck title"
+        />
+      <TextButton
+        style={[styles.button, styles.buttonBlack]}
+        onPress={console.log('oi')}>
+          Submit
+      </TextButton>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  newDeckHeader: {
+    fontSize: 50,
+    textAlign: 'center',
+  },
+  deckTitle: {
+    marginTop: 30,
+    width: '90%',
+    height: 50,
+    fontSize: 18,
+    borderColor: black,
+    borderWidth: 0.5,
+  },
+  button: {
+    marginTop: 30,
+    fontSize: 25,
+    width: 200,
+    padding: 15,
+    borderColor: black,
+    borderRadius: 6,
+    borderWidth: 0.5,
+  },
+  buttonBlack: {
+    backgroundColor: black,
+    color: white,
+  },
+})
 
 export default NewDeck
